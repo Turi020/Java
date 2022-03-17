e6bca8f3e3514216d4a2ad1038af80d8bf532296
Turi020 <turancan.dur@student.htldornbirn.at>
2022-03-17 16:03:09 +0100

    init ran

---
+++ src/IfBedingungen1.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+public class IfBedingungen1 {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        int randomNumber = random.nextInt(100);
+
+        if( (randomNumber > 50) && (randomNumber<90)) {
+
+        } else if(randomNumber > 10) {
+        } else {
+
+
+        }
+}
---
+++ src/Structure3.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+public class Structure3 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int randomNumber = random.nextInt( 100);
+        if ((randomNumber>50) && (randomNumber<90)){
+
+        } else if (randomNumber<10){
+
+        }else{}
+
+
+
+    }
+}
