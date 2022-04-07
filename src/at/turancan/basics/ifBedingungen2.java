a21586e5288be2f784149c1726132bd8f0aa706c
Turi020 <turancan.dur@student.htldornbirn.at>
2022-04-07 16:07:14 +0200

    IfBedingungen1

---
+++ src/at/turancan/basics/ifBedingungen2.java
@@ -0,0 +1,23 @@
+package at.turancan.basics;
+import java.util.Random;
+
+
+public class ifBedingungen2 {
+
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+
+        Random random = new Random();
+        int randomNumber = random.nextInt( 100);
+        System.out.println(randomNumber);
+
+        if(randomNumber<20){
+            System.out.println("Mini");
+        } else if ((randomNumber>=20) && (randomNumber<=50)){
+            System.out.println("Medium");
+        } else if(randomNumber>50){
+            System.out.println("Large");
+        }
+    }
+}
