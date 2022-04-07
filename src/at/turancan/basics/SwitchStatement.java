42fd70c18dc8f9a4407cb075e5a5643ff25a2e0e
Turi020 <turancan.dur@student.htldornbirn.at>
2022-04-07 16:16:16 +0200

    IfBedingungen1

---
+++ src/at/turancan/basics/SwitchStatement.java
@@ -0,0 +1,27 @@
+package at.turancan.basics;
+
+import java.util.Random;
+
+public class SwitchStatement {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        int randomNumber = random.nextInt( 5);
+
+        System.out.println("First number: " + randomNumber);
+        switch (randomNumber){
+            case 0:
+                System.out.println("is 0");
+                break;
+            case 1:
+                System.out.println("is 1");
+                break;
+            case 2:
+                System.out.println("is 2");
+                break;
+            default:
+                System.out.println("other value");
+                break;
+        }
+    }
+}
