9b6d62ca655088a56ef8eda276becacd55d5e619
Turi020 <turancan.dur@student.htldornbirn.at>
2022-04-28 15:34:46 +0200

    whileloop 1

--- src/at/turancan/basics/loop3.java
+++ src/at/turancan/basics/loop3.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class loop3 {
     public static void main(String[] args) {
-        for (int i = 1; i <= 1000; i=i+2) {
+        for (int i = 2; i <= 1000; i=i+2) {
             System.out.println(i);
         }
     }
