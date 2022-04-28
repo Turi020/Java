43dd37257cde851d8948884c9407e1406c10f695
Turi020 <turancan.dur@student.htldornbirn.at>
2022-04-28 15:35:58 +0200

    whileloop 1

---
+++ src/at/turancan/basics/Whileloop1.java
@@ -0,0 +1,20 @@
+package at.turancan.basics;
+
+import java.util.Random;
+
+public class Whileloop1 {
+    public static void main(String[] args) {
+        Random random =new Random();
+        boolean isFinished = false;
+        while ( !isFinished ) {
+            int randomValue = random.nextInt(100);
+            System.out.println("Nr:" + randomValue);
+            if (randomValue>90); {
+                isFinished = true;
+            }
+        }
+        System.out.println( "Finished");
+    }
+
+
+}
