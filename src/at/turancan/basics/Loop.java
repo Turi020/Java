9b0ab26abc3bc4491cbcd6b8a2a8bd102b04afde
Turi020 <turancan.dur@student.htldornbirn.at>
2022-04-28 15:07:19 +0200

    Loop Exercises

---
+++ src/at/turancan/basics/Loop.java
@@ -0,0 +1,14 @@
+package at.turancan.basics;
+
+import java.util.Random;
+
+public class Loop {
+    public static void main(String[] args) {
+
+        for(int i = 10; i >0; i--){
+            System.out.println("Hello " + i);
+        }
+
+    }
+}
+
---
+++ src/at/turancan/basics/loop2.java
@@ -0,0 +1,15 @@
+package at.turancan.basics;
+
+import java.util.Random;
+
+public class loop2 {
+    public static void main(String[] args) {
+
+        for(int i = 1; i <= 100; i++){
+            System.out.println(i);
+        }
+}
+
+
+    }
+
