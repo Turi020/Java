2383ece98eebe64bf82131aa1f98d9ece5b29f09
Turi020 <turancan.dur@student.htldornbirn.at>
2022-10-18 10:11:20 +0200

    Quersumme

--- src/at/turancan/basics/quersumme1.java
+++ src/at/turancan/basics/quersumme1.java
@@ -3,15 +3,18 @@ package at.turancan.basics;
 public class quersumme1 {
 
     public static void main(String[] args) {
-        for (int cnt = 0; cnt < 1001; cnt++) {
+        for (int cnt = 0; cnt < 1000; cnt++) {
             String numberAsString = Integer.toString(cnt);
             char[] numbers = numberAsString.toCharArray();
 
             int sum = 0;
-            for (char number : numbers) {
-                sum = sum + Character.getNumericValue(number);
+            for (int i = 0; i < numbers.length; i++) {
+                sum = sum + Character.getNumericValue(numbers[i]);
             }
-            System.out.println(cnt + " - " + sum);
+
+            int rest = sum % 7;
+            if (rest == 0) {
+                System.out.println("numbers: " + cnt + " Quersumme: " + sum);
+            }
         }
-    }
-}
+    }}
