c8bffbcdac1d6a6c25dd9b53a645e10bd45fcd43
Turi020 <turancan.dur@student.htldornbirn.at>
2022-12-06 10:14:01 +0100

    OO Car

---
+++ src/at/turancan/oo/Main.java
@@ -0,0 +1,13 @@
+package at.turancan.oo;
+
+public class Main {
+    public static void main(String [] args){
+        int a = 7;
+
+    Car c1 = new Car();
+    c1.brand = "Mercedes";
+    c1.serialnumber= "C63";
+    c1.fuelconsumption="6";
+
+    }
+}
---
+++ src/at/turancan/oo/OO_car.java
@@ -0,0 +1,9 @@
+package at.turancan.oo;
+
+public class OO_car {
+
+    public int fuelConsumption;
+    public String brand;
+    public String serialnumber;
+    private String color;
+}
