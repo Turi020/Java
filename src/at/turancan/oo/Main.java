91bcb4798e420fd9e55f2ffc8888736e80af81b2
Turi020 <turancan.dur@student.htldornbirn.at>
2023-01-17 11:02:12 +0100

    OO Car

--- src/at/turancan/oo/Main.java
+++ src/at/turancan/oo/Main.java
@@ -1,33 +1,49 @@
-import at.turancan.oo.Car;
+package at.turancan.oo;
 
 public class Main {
-    public static void main(String [] args){
-        int a = 7;
+ public static void main(String[] args) {
 
-        Car c1 = new Car();
-        c1.brand = "Mercedes";
-        c1.fuelConsumption=7;
-        c1.fuelAmount= 30;
-        c1.serialNumber= "C63";
+  Engine e1 = new Engine(232, Engine.TYPE.DIESEL);
+  Car c1 = new Car(e1, "Volkswagen", "4");
+  c1.setBrand("Volkswagen");
+  c1.setBrake("no");
+  c1.setDrive("yes");
+  c1.setFuelConsumption(38);
+  c1.setSerialNumber("4");
+  c1.setColor("yellow");
+  c1.setFuelAmount(100);
+  c1.amountOfRepetitions = 5;
+  c1.setSpeed(70);
+  System.out.println(c1.getEngine().getHorsePower());
 
-
-
-        Car c2 = new Car();
-        c2.brand = "Audi";
-        c2.fuelConsumption=4;
-        c2.fuelAmount=20;
-        c2.serialNumber= "C6233";
-
-        System.out.println(c2.fuelAmount);
-        c2.drive();
-        System.out.println(c2.fuelAmount);
+  c1.drive();
 
 
 
+  Car c2 = new Car(e1, "Lamborgini", "A12345");
+  c2.setBrand("Lamborgini");
+  c2.setDrive("Yes");
+  c2.setBrake("NO");
+  c2.setFuelConsumption(11);
+  c2.setSerialNumber("A12345");
+  c2.setColor("red");
+  c2.setFuelAmount(78);
+  c2.amountOfRepetitions = 2;
 
+  c2.drive();
+  c1.honk();
+  c1.getRemainingRange();
+  c2.getRemainingRange();
 
-        System.out.println(c1.serialNumber);
-        System.out.println(c2.brand);
-
-    }
+ }
 }
+
+
+
+
+
+
+
+
+
+
