ffb404751e2eb1a430c279dc487c65539488084a
Turi020 <turancan.dur@student.htldornbirn.at>
2023-03-14 10:27:50 +0100

    Car 2.0

--- src/at/turancan/basics/xxx/examples/cars/Car.java
+++ src/at/turancan/basics/xxx/examples/cars/Car.java
@@ -3,17 +3,28 @@ package at.turancan.basics.xxx.examples.cars;
 public class Car {
     private int basePrice;
     private String color;
-    private int baseconsumption;
+    private int baseConsumption;
     private int range;
-    private int maxspeed;
-    private int manufacturerdiscount;
+    private int maxSpeed;
+    private int manufacturerDiscount;
 
     public Car (String c, int maxS, int baseP, int baseC){
         this.color=c;
-        this.maxspeed=maxS;
-        this.baseconsumption=baseP;
+        this.maxSpeed=maxS;
+        this.baseConsumption=baseP;
         this.basePrice=baseP;
 
+    }
+
+    public void Price (){
+        this.basePrice=80000;
+        this.manufacturerDiscount = 4;
+        this.baseConsumption=412;
+        this.range=40000;
+        if (this.range<50000){
+            System.out.println(this.baseConsumption);
+
+        } else if (this.range>=50000){
     }
 
 }
