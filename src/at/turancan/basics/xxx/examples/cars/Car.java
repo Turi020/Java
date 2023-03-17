ae5fd6b9694db28177be1f5a013c70c8e8808d80
Turi020 <turancan.dur@student.htldornbirn.at>
2023-03-17 18:09:23 +0100

    fertig gestellt car 2.0

--- src/at/turancan/basics/xxx/examples/cars/Car.java
+++ src/at/turancan/basics/xxx/examples/cars/Car.java
@@ -19,12 +19,51 @@ public class Car {
     public void Price (){
         this.basePrice=80000;
         this.manufacturerDiscount = 4;
-        this.baseConsumption=412;
+        this.baseConsumption=41;
         this.range=40000;
         if (this.range<50000){
             System.out.println(this.baseConsumption);
 
         } else if (this.range>=50000){
+            System.out.println(this.baseConsumption*9.8);
+        }
+        double discountprice= (this.basePrice/this.manufacturerDiscount);
+        double endprice = this.basePrice - discountprice;
+        System.out.println("Das Auto kostet"+endprice+ "€");
     }
 
-}
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getMaximumSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaximumSpeed(int maximumSpeed) {
+        this.maxSpeed = maximumSpeed;
+    }
+
+    public int getBasePrice() {
+        return basePrice;
+    }
+
+    public void setBasePrice(int basePrice) {
+        this.basePrice = basePrice;
+    }
+
+    public int getBaseConsumption() {
+        return baseConsumption;
+    }
+
+    public void setBaseConsumption(int baseConsumption) {
+        this.baseConsumption = baseConsumption;
+    }
+};
+
+
+
--- src/at/turancan/basics/xxx/examples/cars/Main.java
+++ src/at/turancan/basics/xxx/examples/cars/Main.java
@@ -3,9 +3,9 @@ package at.turancan.basics.xxx.examples.cars;
 public class Main {
     public static void main (String [] args) {
 
-        Manufacturer Mercedes = new Manufacturer("Mercedes Benz Schneider","Schweiz", "2");
-        Engine e1 = new Engine (4,500);
-        Car c1 = new Car (e1,Mercedes,80000,412,"white","Mercedes C63 AMG");
+        Manufacturer Mercedes = new Manufacturer("Mercedes Benz Schneider","Schweiz", 4);
+        Car c1 = new Car ("schwarz",390,80000,41);
+        c1.Price();
 
     }
 }
