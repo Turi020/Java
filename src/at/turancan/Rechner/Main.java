b11fbf34b7057084532bd73e694cdc56891d190e
Turi020 <turancan.dur@student.htldornbirn.at>
2023-05-02 12:02:29 +0200

    Rechner begonnen

---
+++ src/at/turancan/Rechner/Main.java
@@ -0,0 +1,9 @@
+package at.turancan.Rechner;
+
+public class Main {
+    public static void main(String[] args){
+        RootCalculator rootcalc = new RootCalculator();
+        Calculator cal= new Calculator();
+        EconomicalCalculator Eco = new EconomicalCalculator();
+    }
+}
---
+++ src/at/turancan/Rechner/RootCalculator.java
@@ -0,0 +1,4 @@
+package at.turancan.Rechner;
+
+public class RootCalculator {
+}
