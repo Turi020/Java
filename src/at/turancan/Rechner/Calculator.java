33640b96076d3129a44321fdf43962f5307834de
Turi020 <turancan.dur@student.htldornbirn.at>
2023-05-05 13:36:05 +0200

    Rechner begonnen

--- src/at/turancan/Rechner/Calculator.java
+++ src/at/turancan/Rechner/Calculator.java
@@ -1,4 +1,9 @@
 package at.turancan.Rechner;
 
 public class Calculator {
-}
+    public double addition ( double number1, double number2){
+        return number1 + number2;
+    }
+    public double division ( double number1, double number2){
+        return number1 / number2;
+}}
