fb045eab4e208358e3d1f2433d51e05acd505390
Turi020 <turancan.dur@student.htldornbirn.at>
2023-05-15 20:55:23 +0200

    Konto begonnen

--- src/at/turancan/Konto/Creditaccount.java
+++ src/at/turancan/Konto/Creditaccount.java
@@ -3,5 +3,19 @@ package at.turancan.Konto;
 public class Creditaccount {
     private double balance;
 
-    public Creditaccount(double amount, double interstRate, double yearFactor)
+    public Creditaccount(double amount, double interstRate, double yearFactor) {
+        this.balance-= amount *Math.pow((1+ interstRate/100), yearFactor);
+    }
+    public void payCredit (double amount) {
+        this.balance += amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
+
