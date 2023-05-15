22fd24aac3036ff2d65de66f1638ae58794269dd
Turi020 <turancan.dur@student.htldornbirn.at>
2023-05-15 22:02:31 +0200

    Konto fertig

--- src/at/turancan/Konto/Main.java
+++ src/at/turancan/Konto/Main.java
@@ -1,4 +1,12 @@
 package at.turancan.Konto;
 
 public class Main {
+    public static void main ( String [] args){
+Baseaccount base = new Baseaccount();
+Checkingaccount checking = new Checkingaccount(100);
+Savingsbook save = new Savingsbook();
+
+save.deposit(100);
+save.withdraw(100);
+    }
 }
