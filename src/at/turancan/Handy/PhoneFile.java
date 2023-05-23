af8a4a5ff3500e0bc56cc7c545e1cd9940680773
Turi020 <turancan.dur@student.htldornbirn.at>
2023-05-23 11:25:55 +0200

    Phone angefangen

--- src/at/turancan/Handy/PhoneFile.java
+++ src/at/turancan/Handy/PhoneFile.java
@@ -1,4 +1,22 @@
 package at.turancan.Handy;
 
 public class PhoneFile {
+    private String extension;
+    private String persname;
+    private int size;
+
+    public PhoneFile(String extension, int size, String persname){
+        this.extension=extension;
+        this.persname=persname;
+        this.size=size;
+    }
+    public String getExtension(){
+        return extension;
+    }
+    public String getPersname(){
+        return persname;
+    }
+    public int getSize(){
+        return size;
+    }
 }
--- src/at/turancan/Handy/Sim.java
+++ src/at/turancan/Handy/Sim.java
@@ -1,4 +1,24 @@
 package at.turancan.Handy;
 
 public class Sim {
+    private int ID;
+    private String number;
+
+    public Sim( String number, int ID){
+        this.number=number;
+        this.ID=ID;
+    }
+    public void docall(String number){
+        System.out.println("SIM" + ID + "" + this.number + "is calling");
+    }
+
+    public void setNumber (String number){
+        this.number=number;
+    }
+    public int getID(){
+        return ID;
+    }
+    public String getNumber(){
+        return number;
+    }
 }
